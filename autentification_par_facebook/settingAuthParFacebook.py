694d3822234137a034a73c38bb204d551392c887
U-DESKTOP-HB796GP\ccbal <ccbalexandretabot@orange.fr>
2017-03-03 15:33:17 +0100

    En cours authentification par Facebook

---
+++ autentification_par_facebook/settingAuthParFacebook.py
@@ -0,0 +1,8 @@
+FACEBOOK_APP_ID = ''
+FACEBOOK_API_KEY = ''
+FACEBOOK_API_SECRET = ''
+FACEBOOK_REDIRECT_URI = 'http://example.com/login/'
+
+AUTHENTICATION_BACKENDS = (
+    '<app_name>.backends.FacebookBackend',
+)
